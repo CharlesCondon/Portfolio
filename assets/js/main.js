4faa0227efd6391458e4ef570c08358bef407aed
Charles Condon <charlescon@live.com>
2023-03-15 20:40:01 -0400

    start for other pages

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+document.addEventListener('click', function(e) {
+  var el = e.target;
+
+  while (el && !el.href) {
+    el = el.parentNode;
+  }
+
+  if (el) {
+    e.preventDefault();
+    return;
+  }
+});
+  
