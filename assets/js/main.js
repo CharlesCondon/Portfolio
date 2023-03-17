d02eaa340efe8aa6cee74b1e9f10f7605609ef2e
Charles Condon <charlescon@live.com>
2023-03-17 13:49:38 -0400

    contact form and bio added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,28 @@
-document.addEventListener('click', function(e) {
-  var el = e.target;
+// document.addEventListener('click', function(e) {
+//   var el = e.target;
 
-  while (el && !el.href) {
-    el = el.parentNode;
-  }
+//   while (el && !el.href) {
+//     el = el.parentNode;
+//   }
 
-  if (el) {
-    e.preventDefault();
-    return;
-  }
+//   if (el) {
+//     e.preventDefault();
+//     return;
+//   }
+// });
+
+barba.init({
+    transitions: [{
+        name: 'opacity-transition',
+        leave(data) {
+            return gsap.to(data.current.container, {
+            opacity: 0
+            });
+        },
+        enter(data) {
+            return gsap.from(data.next.container, {
+            opacity: 0
+            });
+        }
+    }]
 });
-
-const swup = new Swup();
-  
